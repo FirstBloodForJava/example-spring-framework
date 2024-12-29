e970ccdbc5d1cb77c9fb368c4c33ce0e55dd0ced
oycm <1164864987@qq.com>
2024-12-29 17:51:17 +0800

    JDK代理已经创建目标对象，创建代理对象不会执行无参构造方法

--- src/main/java/com/example/proxy/ChinesePeople.java
+++ src/main/java/com/example/proxy/ChinesePeople.java
@@ -1,13 +1,17 @@
 package com.example.proxy;
 
-import com.example.proxy.People;
-
 /**
  * @author ouyangcm
  * create 2024/12/26 14:29
  */
 public class ChinesePeople implements People {
 
+    {
+        System.out.println("实例代码块");
+    }
+    public ChinesePeople() {
+        System.out.println("ChinesePeople 无参构造执行");
+    }
     @Override
     public void say(String msg) {
         System.out.println(msg);
